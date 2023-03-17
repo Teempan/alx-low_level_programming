79e3fbc5ce4352aa65425d8512c75cfce01c258e
Teempan <125438924+Teempan@users.noreply.github.com>
2023-03-16 17:43:41 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+	* main - entre point
+	* Description: 'the programe's description'
+	* Return: always 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
