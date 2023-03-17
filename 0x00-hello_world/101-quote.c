eb172f7a264f13efe7d8b02e5d0bf2980839b764
Teempan <125438924+Teempan@users.noreply.github.com>
2023-03-16 19:05:35 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
 * main - entre point
 * Description: 'the programe's description'
-* Return: always 0 (Success)
+* Return: always 1 (Success)
 */
 int main(void)
 {
-printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
