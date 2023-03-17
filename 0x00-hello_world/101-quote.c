4e2eba23d993c2de2b145d862f78d1e51bd7180c
Teempan <125438924+Teempan@users.noreply.github.com>
2023-03-16 18:44:40 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - entre point
+* Description: 'the programe's description'
+* Return: always 0 (Success)
+*/
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
