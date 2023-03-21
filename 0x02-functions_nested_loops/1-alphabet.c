4dad0a16e76e45fbeafc1c41573287025b82bbe2
Teempan <125438924+Teempan@users.noreply.github.com>
2023-03-21 16:01:50 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point ,this prints alphabet
+ * in lowercase
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+	{
+		char alpha;
+
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		_putchar(alpha);
+
+		_putchar('\n');
+	}
