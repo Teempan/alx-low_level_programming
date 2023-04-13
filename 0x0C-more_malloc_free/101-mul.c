7c15049edbb7068af6e38338aa429a010b29e801
HTeem <125438924+Teempan@users.noreply.github.com>
2023-04-13 19:09:23 +0000

    test15

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,178 +1,99 @@
-#include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
 
 /**
- * _calloc - calloc function
- * @nmemb: number of elements
- * @size: size of each element
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
  *
- * Return: pointer to the memory space
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+int is_digit(char *s)
 {
-	unsigned int i;
-	char *ptr;
+	int i = 0;
 
-	if (size == 0 || nmemb == 0)
-		return (NULL);
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
-		return (NULL);
-	for (i = 0; i < nmemb * size; ++i)
-		ptr[i] = 0;
-	return (ptr);
-}
-
-/**
- * _strlen - length of string
- * @s - the string
- *
- * Return: length of the string
- */
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; s[i]; ++i)
-		;
-	return (i);
-}
-
-/**
- * _print - prints using putchar
- * @str: string to print
- *
- * Return: no value
- */
-void _puts(char *str)
-{
-	while (*str)
-		_putchar(*(str++));
-}
-
-/**
- * _isdigit
- * @s1: string
- *
- * Return: 0 on success, 1 if fail
- */
-int _strdigit(char *s1)
-{
-	if (*s1 == '-' || *s1 == '+')
-		++s1;
-	while (*s1)
+	while (s[i])
 	{
-		if (*s1 < '0' || *s1 > '9')
-		{
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
-		}
-		++s1;
+		i++;
 	}
 	return (1);
 }
 
 /**
- * reverse- reverse string
- * @first: first index
- * @last: last index
- * @str: string to reverse
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
- * Return: no value
+ * Return: the length of the string
  */
-void rev_num_str(int first, int last, char *str)
+int _strlen(char *s)
 {
-	int i, j;
-	char tmp;
+	int i = 0;
 
-	while (str[last] == 0 && last != first)
-		--last;
-	for (i = first, j = last; i <= j; ++i, --j)
+	while (s[i] != '\0')
 	{
-		tmp = str[i] + '0';
-		str[i] = str[j] + '0';
-		str[j] = tmp;
+		i++;
 	}
+	return (i);
 }
 
 /**
- * multiply - multiply a long string
- * @s1: string 1
- * @s2: string 2
- *
- * Return: pointer to answer
+ * errors - handles errors for main
  */
-char *strmul(char *s1, char *s2)
+void errors(void)
 {
-	int len1, len2, i, j, a, b, neg = 0;
-	char *res;
-	char mul, mul_carry, sum, sum_carry;
-
-	if (*s1 == '-')
-	{
-		neg ^= 1;
-		++s1;
-	}
-	if (*s2 == '-')
-	{
-		neg ^= 1;
-		++s2;
-	}
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	res = _calloc(len1 + len2 + 1 + neg, sizeof(char));
-	if (res == NULL)
-		return (NULL);
-	if (neg)
-		res[0] = '-';
-	for (i = len2 - 1, a = neg; i >= 0; --i, ++a)
-	{
-		mul_carry = 0;
-		sum_carry = 0;
-		for (j = len1 - 1, b = a; j >= 0; --j, ++b)
-		{
-			mul = (s1[j] - '0') * (s2[i] - '0') + mul_carry;
-			mul_carry = mul / 10;
-			mul %= 10;
-			sum = res[b] + mul + sum_carry;
-			sum_carry = sum / 10;
-			sum %= 10;
-			res[b] = sum;
-		}
-		res[b] = sum_carry + mul_carry;
-	}
-	rev_num_str(neg, b, res);
-	return (res);
+	printf("Error\n");
+	exit(98);
 }
 
 /**
- * main - man fxn
- * @argc: argument count
- * @argv: string of arguments
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: exit status
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	char *ans;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	if (argc != 3)
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		_puts("Error\n");
-		exit(98);
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-	if (!_strdigit(argv[1]) || !_strdigit(argv[2]))
+	for (i = 0; i < len - 1; i++)
 	{
-		_puts("Error\n");
-		exit(98);
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
 	}
-	ans = strmul(argv[1], argv[2]);
-	if (ans == NULL)
-	{
-		_puts("Error\n");
-		exit(98);
-	}
-	_puts(ans);
+	if (!a)
+		_putchar('0');
 	_putchar('\n');
-	free(ans);
-	exit(EXIT_SUCCESS);
+	free(result);
+	return (0);
 }
